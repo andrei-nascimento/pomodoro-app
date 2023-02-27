f1eecf612cb9f8855050bda05e35f8ae99a76019
Andrei Nascimento <andrei.lima.nas@outlook.com>
2023-02-27 11:15:47 -0300

    Criação inicial das pastas e arquivos do projeto

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text>Hello World</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    },
+});
